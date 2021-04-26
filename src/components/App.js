380f4d8d595c301c4adb44c32ee9d25fbaa09dec
jubi-in <thetruthrunswild@naver.com>
2021-04-26 16:04:40 +0900

    auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "myFb";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
-    <AppRouter />
+    <>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    <footer>&copy; Jwitter {new Date().getFullYear()}</footer>
+    </>
   );
 }
 
